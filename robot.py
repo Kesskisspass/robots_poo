d88f0ac2ebb99a210140cebf525a104372a8259e
kesskisspass <kesskisspass@gmail.com>
2020-02-06 15:26:16 +0100

    Add files, class robotNG

--- robot.py
+++ robot.py
@@ -1,34 +1,32 @@
 class Robot:
 
     """
-    Classe des robots
+    Classe des robots 1ère génération
     """
 
     direction=["Nord","Est","Sud","Ouest"]
 
-    def __init__(self,nom,x=0,y=0,index_direction=2)
+    def __init__(self,nom,x=0,y=0,index_direction=1):
         self.nom = nom
         self.x = x
         self.y = y
         self.direction = index_direction
 
-    def __avance():
-        if(self_direction == 1):
+    def _avance(self):
+        if(self.direction == 0):
             self.x += 1
-        elif(self.direction == 2):
+        elif(self.direction == 1):
             self.y += 1
-        elif(self.direction == 3):
+        elif(self.direction == 2):
             self.x -= 1
         else:
             self.y -= 1
 
-    def __droite():
-        if(self.direction < 4):
+    def _droite(self):
+        if(self.direction < 3):
             self.direction += 1
             return
         self.direction = 0
 
-    def __etat():
-        print(f"Position x:{self.x}, y: {self.y}, Direction: {self.direction}, ")
-
-    
+    def _etat(self):
+        print(f"Position x:{self.x}, y: {self.y}, Direction: {Robot.direction[self.direction]} ")
---
+++ robotng.py
@@ -0,0 +1,9 @@
+from robot import Robot
+
+class RobotNG(Robot):
+    """Classe des robots de nouvelle génération"""
+
+    def __init__(self,nom):
+        Robot.__init__(self, nom)
+
+    
