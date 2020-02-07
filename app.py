da82881cfbd6cc69d4c457742dd9587a3b5099ce
kesskisspass <kesskisspass@gmail.com>
2020-02-07 12:29:05 +0100

    quick fix

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from robot import Robot
 from robotng import RobotNG
 
+# Le joueur choisit le nom de son robot
 name = input("Veuillez créer un robot, choisissez un nom: \n")
 
+# Puis son type (Robot ou RobotNG)
 type = 0
 while((type != "1") | (type != "2")):
     type = input("Bien, veuillez maintenant choisir le type de robot:\n -'1' pour normal \n -'2' pour nouvelle génération\n")
@@ -14,27 +16,33 @@ while((type != "1") | (type != "2")):
         break
     else:
         print("Veuillez choisir 1 ou 2 !")
+# Affichage de l'état initial
 robot._etat()
 
-action = input("Veuillez entrer une action:\n - 'a' pour Avancer,\n - 'g' pour aller à Gauche,\n - 'd' pour aller à Droite,\n - 't' pour activer le Turbo !\n - 'q' pour Quitter \n")
+if(robot.__class__.__name__ == "Robot"):
+    print("Classe 1")
+    actions = input("Veuillez entrer une action:\n - 'a' pour Avancer,\n - 'd' pour aller à Droite,\n - 'q' pour Quitter \n")
+else:
+    print("Classe 2")
+    actions = input("Veuillez entrer une action:\n - 'a' pour Avancer,\n - 'g' pour aller à Gauche,\n - 'd' pour aller à Droite,\n - 't' pour activer le Turbo !\n - 'q' pour Quitter \n")
+
+
+# Proposition des actions disponibles
+action = actions
 
 while((action != "a") | (type != "g") | (action != "d") | (type != "t") | (type != "q")):
     if(action != "q"):
         if(action == "a"):
             robot._avance()
-            action = input("Veuillez entrer une action:\n - 'a' pour Avancer,\n - 'g' pour aller à Gauche,\n - 'd' pour aller à Droite,\n - 't' pour activer le Turbo !\n - 'q' pour Quitter \n")
-
+            action = actions
         elif(action == "g"):
             robot._gauche()
-            action = input("Veuillez entrer une action:\n - 'a' pour Avancer,\n - 'g' pour aller à Gauche,\n - 'd' pour aller à Droite,\n - 't' pour activer le Turbo !\n - 'q' pour Quitter \n")
-
+            action = actions
         elif(action == "d"):
             robot._droite()
-            action = input("Veuillez entrer une action:\n - 'a' pour Avancer,\n - 'g' pour aller à Gauche,\n - 'd' pour aller à Droite,\n - 't' pour activer le Turbo !\n - 'q' pour Quitter \n")
-
-        else:
-            robot._switch_turbo()
-            action = input("Veuillez entrer une action:\n - 'a' pour Avancer,\n - 'g' pour aller à Gauche,\n - 'd' pour aller à Droite,\n - 't' pour activer le Turbo !\n - 'q' pour Quitter \n")
+            action = actions
     else:
-        print("*****\nVous quittez le jeu, au revoir !\n*****")
-        break
+        robot._switch_turbo()
+        action = actions
+    print("*****\nVous quittez le jeu, au revoir !\n*****")
+    break
